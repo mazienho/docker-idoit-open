7b7bf25fc3845966bf023dee54b4ce7b61368ad4
mazienho <mazienho@gmx.de>
2015-01-18 23:32:08 +0100

    Add patch files. Modified config script.

--- src/set_idoit_configs.sh
+++ src/set_idoit_configs.sh
@@ -5,14 +5,14 @@
 
 #Check if patch file(s) exists
 
-if [ -f /src/my-cnf.patch ]; then
+if [ -f /idoit-src/my-cnf.patch ]; then
 	patch /etc/mysql/my.cnf < /src/my-cnf.patch
 	echo "MySQL config patched"
 else
 	echo "No mysql patch file found. Modify it manually."
 fi
 
-if [ -f /src/php-ini.patch ]; then
+if [ -f /idoit-src/php-ini.patch ]; then
 	patch /etc/php5/apache2/php.ini < /src/php-ini.patch
 	echo "MySQL config patched"
 else
