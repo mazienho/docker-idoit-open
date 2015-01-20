bd2421f42bcecd7c095ddee2893b93e16cace287
mazienho <mazienho@gmx.de>
2015-01-20 22:12:11 +0100

    Update zip file added in install script.

--- src/install-idoit.sh
+++ src/install-idoit.sh
@@ -2,6 +2,7 @@
 
 instfile=/idoit-src/idoit-open-1.4.7.zip
 updtfile=/idoit-src/idoit-open-1.4.8-update.zip
+aptwget=0
 
 apt-get update
 apt-get install -y apache2 libapache2-mod-php5 php5 php5-cli php5-xmlrpc php5-ldap php5-gd php5-mysql php5-mcrypt php5-snmp mysql-server openssh-server unzip sudo patch
@@ -9,12 +10,14 @@ apt-get install -y apache2 libapache2-mod-php5 php5 php5-cli php5-xmlrpc php5-ld
 echo "root:root" | chpasswd
 
 mkdir -p /var/www/i-doit
-if [ -f $instfile ]; then
-	unzip -qq /idoit-src/idoit-open-1.4.7.zip -d /var/www/i-doit
-else
+if [ ! -f $instfile ]; then
+	aptwget=1
 	apt-get install -y wget
-	wget http://sourceforge.net/projects/i-doit/files/i-doit/1.4.7/idoit-open-1.4.7.zip/download -O $instfile
+	wget http://sourceforge.net/projects/i-doit/files/i-doit/1.4.7/idoit-open-1.4.7.zip -O $instfile
 fi
+#	unzip -qq /idoit-src/idoit-open-1.4.7.zip -d /var/www/i-doit
+	unzip -qq $instfile -d /var/www/i-doit
+
 cd /var/www/i-doit 
 chown -R www-data:www-data .
 chmod +x idoit-rights.sh
@@ -34,21 +37,23 @@ if [ $? == 0 ]; then
 	echo "Now setting up recommended configs"
 	echo ""
 	/idoit-src/set_idoit_configs.sh
-	echo "Removing install files"
-	mv /idoit-src/run.sh /run.sh
-	rm -rf /idoit-src
 	echo ""
 fi
 
 #Updates
-#echo ""
-#if [ -f $updtfile ]; then
-#	unzip -qq -uo $updtfile -d /var/www/i-doit
-#	echo "Local update zip file found. Open <i-doit-IP/updates> in webbrowser."
-#else
-#	wget http://sourceforge.net/projects/i-doit/files/i-doit/1.4.8/idoit-open-1.4.8-update.zip -O $updtfile
-#	unzip -qq -uo $updtfile -d /var/www/i-doit
-#fi
+echo ""
+echo "Installing Updates"
+if [ ! -f $updtfile ]; then
+	if [ $aptwget == 0 ]; then
+		apt-get install -y wget
+	fi
+	wget http://sourceforge.net/projects/i-doit/files/i-doit/1.4.8/idoit-open-1.4.8-update.zip -O $updtfile
+	unzip -qq -uo $updtfile -d /var/www/i-doit
+	echo "Update zip file extracted. Open <i-doit-IP/updates> in webbrowser and follow instructions."
 
-# We never should end here ;-)
-exit 1
+fi
+echo "Removing install files"
+mv /idoit-src/run.sh /run.sh
+rm -rf /idoit-src
+
+exit 0
